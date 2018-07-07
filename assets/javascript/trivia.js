52f2044eeb94ada09099616a72c82836957e49c3
stellaCmiller <stella.miller@utexas.edu>
2018-07-07 12:27:04 -0500

    gifs?

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -190,7 +190,7 @@ var TriviaGame = {
         $("#answer-container").empty();
         $("#question-container").append(`
         <div><h2>CORRECT!!!!!</h2></div>
-        <img src='./images/marioStarGet.gif'>`);
+        <img src='/images/marioStarGet.gif'>`);
         TriviaGame.numCorrect++;
         TriviaGame.currentQuestion++;
         setTimeout(function(){TriviaGame.displayNextQuestion()}, 6000);
@@ -202,11 +202,11 @@ var TriviaGame = {
         if (timesup){
             $("#question-container").append(`
             <div><h2>Oof, you took too long! The correct answer was: ${a} </h2></div>
-            <img src='./images/linkRagdollGif.gif'>`);
+            <img src='/images/linkRagdollGif.gif'>`);
         } else {
             $("#question-container").append(`
             <div><h2>Sorry, the correct answer was actually: ${a} </h2></div>
-            <img src='./images/linkRagdollGif.gif'>`);
+            <img src='/images/linkRagdollGif.gif'>`);
         }
         TriviaGame.numWrong++;
         TriviaGame.currentQuestion++;
